34ccb2178b2135bbcba05d36175f33336b9c0dcb
ali moharrami <alimoharrami251@gmail.com>
2025-07-28 12:51:40 +0330

    [Add]: Add middleware.

---
+++ blog/internal/routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"go-blog/internal/middleware"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+// Route defines the structure for dynamic routing
+type Route struct {
+	Method     string
+	Path       string
+	HandleFunc gin.HandlerFunc
+}
+
+// Controller defines the structure for a controller with routes
+type Controller struct {
+	Routes []Route
+}
+
+// SetupRouter dynamically sets up routes
+func SetRouter(db *gorm.DB) *gin.Engine {
+	gin.SetMode("release")
+
+	r := gin.Default()
+	r.Use(middleware.CORS())
+	r.Use(middleware.Logger())
+
+	return r
+}
