76c8d47811dc13bdbe64435c670a0ac2c708f53a
ali moharrami <alimoharrami251@gmail.com>
2025-07-28 11:36:22 +0330

    [Add]: Add config file.

--- blog/cmd/blog/main.go
+++ blog/cmd/blog/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	handlers "go-blog/internal/api"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"go-blog/internal/config"
 )
 
 func main() {
+	// Load configuration
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	log.Println(cfg.Server.Port)
 	r := gin.Default()
 
 	r.LoadHTMLGlob("../../web/*")
